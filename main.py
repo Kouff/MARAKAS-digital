503d29f24636e59cf571e8fa0912e8beb1505c4e
Kouff <thematuhaimmortal@gmail.com>
2021-01-09 11:34:12 +0200

    add views and routers for retrieving products or a product and adding reviews to one

--- main.py
+++ main.py
@@ -72,16 +72,43 @@ def init_db():
     db.session.commit()
 
 
-class ProductView(views.MethodView):
+class ProductListView(views.MethodView):
+
+    def get(self):
+        products = Product.query.all()
+        return jsonify([product.json() for product in products])
+
+
+class ProductDetailView(views.MethodView):
+
+    def validate_data(self):
+        data = request.form or request.json
+        title = data.get('title')
+        review = data.get('review')
+        error_messages = {}
+        if not title:
+            error_messages['title'] = 'This field may not be blank.'
+        if not review:
+            error_messages['review'] = 'This field may not be blank.'
+        if error_messages:
+            raise InvalidData(error_messages)
+        return data
 
     def get(self, id):
-        return str(id)
+        product = Product.query.get(id)
+        return jsonify(product.json(reviews=True))
 
     def put(self, id):
-        return str(id)
+        data = self.validate_data()
+        product = Product.query.get(id)
+        review = Review(review=data['review'], title=data['title'], product=product)
+        db.session.add(review)
+        db.session.commit()
+        return jsonify(product.json(reviews=True)), 201
 
 
-app.add_url_rule('/<int:id>/', view_func=ProductView.as_view('products'))
+app.add_url_rule('/products/', view_func=ProductListView.as_view('products_list'))
+app.add_url_rule('/products/<int:id>/', view_func=ProductDetailView.as_view('products_detail'))
 
 if __name__ == "__main__":
     engine = db.get_engine()
